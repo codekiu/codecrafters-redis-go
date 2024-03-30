5de2ce9db5e355e7e188bd4b093a24d9b0702988
Jesus Copado Espejo <jc.espejo@offensive-security.com>
2024-03-30 11:19:31 +0000

    attempt 2nd stage

--- app/server.go
+++ app/server.go
@@ -2,23 +2,60 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
+
 	// Uncomment this block to pass the first stage
 	"net"
-	"os"
 )
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
+	port := 6379
+	address := "0.0.0.0"
 
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	fullAddress := address + ":" + strconv.Itoa(port)
+
+	listener, err := net.Listen("tcp", fullAddress)
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
+		return
 	}
-	_, err = l.Accept()
+
+	defer listener.Close()
+
+	fmt.Printf("Server is listening in port: %d\n", port)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			continue
+		}
+
+		// Handle client connection
+		handleClient(conn)
+	}
+}
+
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+
+	// Read data
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		return
+	}
+
+	request := strings.TrimSpace(string(buf[:n]))
+	fmt.Println("Request is: ", request)
+
+	if request == "PING" {
+		str := "+PONG\r\n"
+		response := []byte(str)
+
+		conn.Write(response)
 	}
 }
